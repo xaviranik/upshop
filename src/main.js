a07ac579caa374ab71e8bfd2ff0b00d094f16eee
xavir <xaviranik.me@gmail.com>
2020-07-15 21:02:50 +0600

    Added: Project structure

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import 'bootstrap';
+import 'popper.js';
+import jQuery from 'jquery';
+window.$ = window.jQuery = jQuery;
+
+import './assets/scss/app.scss';
+
 Vue.config.productionTip = false;
 
 new Vue({
